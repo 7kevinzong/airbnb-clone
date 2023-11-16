399419a213f7b3eec90d91fefaf4d21419c37808
Kevin Zong <7kevinzong@gmail.com>
2023-11-16 18:20:23 -0500

    feat(navbar): add logo, search bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,28 @@
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
 
 const font = Nunito({
-  subsets: ['latin']
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
