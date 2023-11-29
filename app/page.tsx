8c1a2092f79e77269c599895dd728c28487697b0
Kevin Zong <7kevinzong@gmail.com>
2023-11-29 14:44:52 -0500

    feat(listings): add card for listings and display available listings as cards on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return <EmptyState showReset />;
@@ -16,7 +19,15 @@ export default async function Home() {
         md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5
         2xl:grid-cols-6 gap-8"
       >
-        My future listings
+        {listings.map((listing: any) => {
+          return (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          );
+        })}
       </div>
     </Container>
   );
