67e129612e04ca1b52e09d4dd1f93d1007b612c3
Kevin Zong <7kevinzong@gmail.com>
2023-11-29 15:49:07 -0500

    feat(listings): show location, category, price for each listing and button to cancel reservations

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
   data: Listing;
@@ -86,6 +87,24 @@ const ListingCard = ({
             <HeartButton listingId={data.id} currentUser={currentUser} />
           </div>
         </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          <div>{!reservation && <div className="font-light">night</div>}</div>
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
       </div>
     </div>
   );
